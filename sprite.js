eb9fc3208064194c0837c59e2af3a0d7eaae5191
Gabriel <gabrielmartinssantana@hotmail.com>
2018-05-18 11:33:07 -0300

    Iniciando a criação dos Sprites Inimigos

--- sprite.js
+++ sprite.js
@@ -1,18 +1,28 @@
 function Sprite() {
+  //Sprite principal
   this.x = 245;
   this.y = 450;
   this.larg = 20;
   this.alt = 20;
-
   this.vx = 0;
   this.vy = 0;
-
   this.ax = 0;
   this.ay = 0;
-
   this.cor = "#4169E1";
-
   this.ang = 270;
+
+  //Sprites inimigos
+  this.ex = 100;
+  this.ey = 0;
+  this.elarg = 20;
+  this.ealt = 20;
+  this.evx = 0;
+  this.evy = 0;
+  this.eax = 0;
+  this.eay = 0;
+  this.ecor = "#DC143C";
+  this.eang = 90;
+  this.eQuant = 0;
 }
 
 Sprite.prototype.desenhar = function(ctx) {
@@ -35,6 +45,30 @@ Sprite.prototype.desenhar = function(ctx) {
     ctx.strokeStyle = "red";
     ctx.lineWidth = 3;
     ctx.strokeRect(-this.larg/2, -this.alt/2, this.larg, this.alt);
+  }
+  ctx.restore();
+}
+
+Sprite.prototype.desenharInimigo = function(ctx) {
+  ctx.fillStyle = this.ecor;
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.save();
+  ctx.translate(this.ex, this.ey);
+  ctx.rotate(this.eang*Math.PI/180);
+  ctx.beginPath();
+  ctx.moveTo(-this.elarg/2, -this.ealt/2);
+  ctx.lineTo(-this.elarg/2, +this.ealt/2);
+  ctx.lineTo(+this.elarg/2 + 10, 0);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+  if(this.debug)
+  {
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(-this.elarg/2, -this.ealt/2, this.elarg, this.ealt);
   }
   ctx.restore();
 }
